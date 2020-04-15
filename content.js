ead9b6a23476cf829e48e79eb0208eda3e25961b
Zoltán Tran <tran.zoltan@protonmail.com>
2020-04-15 11:59:20 +0100

    Removal

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-var normalDictionary = ['COVID', 'covid', 'COVID19', 'covid19', 'coronavirus', 'corona virus', 'Coronavirus', 'pandemic']
+var normalDictionary = ['COVID', 'covid', 'COVID-19', 'covid-19', 'coronavirus', 'corona virus', 'Coronavirus', 'pandemic']
 
 var aggressiveDictionary = ['corona', 'virus', 'crisis', 'distancing', 'apocalypse']
 
@@ -12,28 +12,27 @@ var modes = {
 var selectedMode = modes.AGGRESSIVE // Load from storage (options)
 
 var isDangerous = function (node) {
-  var isSafe = true
   var i;
-  for (i = 0; i < normalDictionary.length; i++) 
+  for (i = 0; i < normalDictionary.length; i++)
     if (node.nodeValue.search(normalDictionary[i]) > 0)
-      isSafe = false
+      return true
   if (selectedMode === modes.AGGRESSIVE)
-    for (i = 0; i < aggressiveDictionary.length; i++) 
+    for (i = 0; i < aggressiveDictionary.length; i++)
       if (node.nodeValue.search(aggressiveDictionary[i]) > 0)
-        isSafe = false
-  return !isSafe
+        return true
+  return false
 }
 
 
 var hideNode = function (node) {
-  console.log(node)
+  node.classList.add('cf-hidden')
 }
 
 var e, c;
 for (e = 0; e < elements.length; ++e) {
   for (c = 0; c < elements[e].childNodes.length; c++) {
     if (elements[e].childNodes[c].nodeType === 3) {
-       if (isDangerous(elements[e].childNodes[c]))
+      if (isDangerous(elements[e].childNodes[c]))
         hideNode(elements[e])
     }
   }
